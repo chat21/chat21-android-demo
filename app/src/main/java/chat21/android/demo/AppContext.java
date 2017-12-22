9a974258f1cfe23d5bc177d4ae2681ad41358da0
andrea leo <andrea.leo83@gmail.com>
2017-12-22 10:42:23 +0100

    added login activity

--- app/src/main/java/chat21/android/demo/AppContext.java
+++ app/src/main/java/chat21/android/demo/AppContext.java
@@ -5,8 +5,14 @@ import android.content.Context;
 import android.support.multidex.MultiDex;
 import android.util.Log;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import chat21.android.core.ChatManager;
+import chat21.android.core.users.models.ChatUser;
+import chat21.android.core.users.models.IChatUser;
 import chat21.android.ui.ChatUI;
+import chat21.android.ui.login.ChatLoginActivity;
 
 /**
  * Created by stefanodp91 on 25/09/17.
@@ -37,12 +43,21 @@ public class AppContext extends Application {
         ChatManager.Configuration mChatConfiguration =
                 new ChatManager.Configuration.Builder(getString(R.string.tenant)).firebaseUrl("https://chat-v2-dev.firebaseio.com/").build();
 
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
         // assuming you have a login, check if the logged user (converted to IChatUser) is valid
-        if (DummyDataManager.getLoggedUser() != null) {
-            ChatManager.start(instance, mChatConfiguration, DummyDataManager.getLoggedUser());
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+
+            IChatUser iChatUser = new ChatUser();
+            iChatUser.setId(currentUser.getUid());
+            iChatUser.setEmail(currentUser.getEmail());
+
+            ChatManager.start(this, mChatConfiguration, iChatUser);
             Log.i(TAG, "chat has been initialized with success");
+
             ChatUI.getInstance().setContext(instance);
             Log.i(TAG, "ChatUI has been initialized with success");
+
         } else {
             Log.w(TAG, "chat can't be initialized because chatUser is null");
         }
